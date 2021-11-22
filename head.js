5abe1783796944d3b87ae27838fae7b5ec7523ec
Ren <cassandra.chen@uoit.net>
2021-11-22 21:46:23 +0000

    improved assertEqual and added head

--- head.js
+++ head.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if(actual !== expected){
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`)
@@ -8,6 +7,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+function head(arr){
+  return arr[0]
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
