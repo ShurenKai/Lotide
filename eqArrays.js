9206e1ce97ce74d13df714384fb8a60b6bbed271
Ren <cassandra.chen@uoit.net>
2021-11-23 22:03:59 +0000

    created a function to compare arrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const eqArrays = function(arr1, arr2) {
+  let bool = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      bool = true;
+    } else {
+      bool = false;
+      process.exit();
+    }
+  }
+  return bool;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`);
+  } else if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `);
+  }
+};
