917391ff4802be0312cef4f2d3f7062f83faef7b
Ren <cassandra.chen@uoit.net>
2021-11-23 22:37:05 +0000

    improved eqArrays to include assertion messages

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function (arr1, arr2) {
+  let bool = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      bool = true;
+    } else {
+      bool = false;
+      process.exit();
+    }
+  }
+  return bool;
+};
+
+const assertArraysEqual = function (arr1, arr2) {
+  let output = eqArrays(arr1, arr2);
+  if (output) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} != ${arr2}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
