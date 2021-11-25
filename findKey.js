f0d18d89cc9df6d0a8112d7eaa1192fe2b432dce
Ren <cassandra.chen@uoit.net>
2021-11-25 21:37:03 +0000

    return name or key based on star review

---
+++ findKey.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if(actual !== expected){
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} != ${expected}`)
+  }
+  else if(actual === expected){
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} `)
+  }
+};
+
+//return a truthy value is key is found
+//return undefined if key isn't found 
+
+const findKey = (obj, callback) => {
+  for(let title in obj){
+    if(callback(obj[title])){
+      return title
+    }
+  }
+}
+
+//test case
+const help = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+console.log(help)
